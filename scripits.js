9fa14d3a1dd57efe1d2db5b4a04827fb16bfde09
Rgomes3333 <raulgc268@outlook.com>
2023-04-19 12:19:18 -0300

    Atualizando, consumindo uma API

--- scripits.js
+++ scripits.js
@@ -1,14 +1,22 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
-const dolar = 5.2
-const euro = 5.9
-const bitcoin = 0.0000067
 
-const convertValue = () => {           
+// consumindo uma api utilizando os recursos do java script
+// função async avisando ao javascript que é uma funcao assincrona, await pedindo pro js esperar ate pegar os dados no servidor
+// asyn só funciona dentro de uma função, tem que avisar na funçao que ela é 
+// forma de utilizar o async fora da arrow async function myFunction () {}
+
+const convertValue = async () => {           
     const inputReais = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+    
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
         { style: 'currency', currency: 'BRL' }
@@ -27,9 +35,9 @@ const convertValue = () => {
     }
 
     if (select.value === 'Bitcoin'){
-        currencyValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        currencyValueText.innerHTML = new Intl.NumberFormat('XBT',
             { style: 'currency', currency: 'BTC' }
-        ).format(inputReais * bitcoin)
+        ).format(inputReais / bitcoin)
     }
 
 }
